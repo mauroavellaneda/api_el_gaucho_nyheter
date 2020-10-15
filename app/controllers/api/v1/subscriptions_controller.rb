50743332669e25b3510c8527024ee4ec22c93406
SebastianN97 <sebbe132132@gmail.com>
2020-10-15 15:18:38 +0200

    Adding subscription line 7 in subscriptions controller. Branching off to development to start on a new feature and later on continue with this one.

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,4 +1,20 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    begin
+      customer_id = get_customer(params[:stripeToken])
+      Subscription = Stripe::Subscription.create({ customer: customer_id, plan: "subscription" })
+    rescue => error
+      
+    end
+  end
+
+  private
+
+  def get_customer(stripe_token)
+    customer = Stripe::Customer.list(email: current_user.email).data.first
+    customer ||= Stripe::Customer.create({ email: current_user.email, source: stripe_token })
+    customer.id
   end
 end
