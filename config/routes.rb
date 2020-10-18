15f3e6a37f95301478479c2b6051579dd90f9328
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-18 17:15:00 +0200

    Removes format json, semaphore complaining

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for "User", at: "api/v1/auth", skip: [:omniauth_callbacks]
-  namespace :api, constraints: { format: 'json'} do
+  namespace :api do
     namespace :v1 do
       resources :articles, only: [:index, :show, :create]
       namespace :admin do
