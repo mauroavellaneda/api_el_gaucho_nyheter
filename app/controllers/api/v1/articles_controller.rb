0914d4a479fa06589571f3b064e9f23d4e7de7db
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-25 20:04:15 +0100

    Formates and implements provisory solution to show articles in local but still this is not the correct implementation

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::ArticlesController < ApplicationController
+  before_action :valid_category?, only: :index, if: :selected_category
+
   def index
-    articles = if params[:location]
-                 Article.where(category: :local).where(location: params[:location])
+    articles = if selected_category
+        Article.where(category: params["category"])
+      elsif selected_location? == true
+        Article.where(location: current_location)
+      else
+        Article.all
+      end
 
-               elsif params[:category]
-                 Article.where(category: params[:category])
-
-               else
-                 Article.all
-               end
     render json: articles, each_serializer: ArticlesIndexSerializer
   rescue StandardError
     render json: { error: "Sorry, we don't have that category" }, status: :not_found
@@ -18,6 +19,27 @@ class Api::V1::ArticlesController < ApplicationController
     article = Article.find(params[:id])
     render json: article, serializer: ShowArticleSerializer
   rescue StandardError => e
-    render json: { error: 'Article does not exist' }, status: :not_found
+    render json: { error: "Article does not exist" }, status: :not_found
+  end
+
+  def valid_category?
+    unless Article.categories.keys.include?(params["category"])
+      render json: { error: "Sorry, we don't have that category" }, status: :not_found
+    end
+  end
+
+  def selected_category
+    !params["category"].nil?
+  end
+
+  def selected_location?
+    !params["local"].nil?
+  end
+
+  def current_location
+    if params["local"] == "Sweden"
+    else
+      "Unites States of America"
+    end
   end
 end
